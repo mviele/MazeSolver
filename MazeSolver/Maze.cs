d4098bacbd27b4a2fc8f73a9d96d1868815eaaae
mviele <mviele@utexas.edu>
2016-10-09 16:54:43 -0500

    first commit

---
+++ MazeSolver/Maze.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace MazeSolver
+{
+    class Maze
+    {
+        private Bitmap mazeImage;
+
+        public Maze(Bitmap mazeImage)
+        {
+            this.mazeImage = mazeImage;
+        }
+
+        public void solve()
+        {
+           
+        }
+    }
+}
---
+++ MazeSolver/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MazeSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap mazeImage = (Bitmap) Image.FromFile(args[0]);
+            Maze maze = new Maze(mazeImage);
+        }
+    }
+}
