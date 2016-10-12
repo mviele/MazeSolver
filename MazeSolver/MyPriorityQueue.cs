bee847e4bb38e611b3988e47f96d88bb959649e4
mviele <mviele@utexas.edu>
2016-10-11 19:56:09 -0500

    Added better comments to Node and MyPriorityQueue classes

--- MazeSolver/MyPriorityQueue.cs
+++ MazeSolver/MyPriorityQueue.cs
@@ -7,15 +7,30 @@ namespace MazeSolver
 {
     class MyPriorityQueue<T> where T : IComparable
     {
+        //heap array
         private T[] list;
+        //tail pointer
         private int size;
 
-        public MyPriorityQueue(int size)
+        /**
+         * O(1)
+         * Creates the heap array with the specified size.
+         * Tail pointer is set to the head of the array, 
+         * signifying an empty queue. 
+         * 
+         **/
+        public MyPriorityQueue(int heapSize)
         {
             this.size = 0;
-            list = new T[size];
+            list = new T[heapSize];
         }
 
+        /**
+         * O(1)
+         * Returns true if the tail pointer points to the 
+         * head of the heap array. 
+         * 
+         **/
         public bool isEmpty()
         {
             if (size == 0)
@@ -25,6 +40,12 @@ namespace MazeSolver
             return false;
         }
 
+        /**
+         * O(log N)
+         * Adds the new element t to the bottom of the heap, 
+         * and then percolates it up to the approriate spot. 
+         * 
+         **/
         public void Enqueue(T t)
         {
             if (size == list.Length - 1)
@@ -41,6 +62,12 @@ namespace MazeSolver
             }
         }
 
+        /**
+         * O(log N)
+         * Returns the highest priority element in the heap, 
+         * and reheapifies the heap. 
+         * 
+         **/
         public T Dequeue()
         {
             if (size == 0)
@@ -55,6 +82,13 @@ namespace MazeSolver
             return value;
         }
 
+        /**
+         * O(1)
+         * Standard swap function, when past two indices in 
+         * the heap array, this function swaps the values of 
+         * those two locations.
+         * 
+         **/
         private void swap(int x1, int x2)
         {
             var temp = list[x1];
@@ -62,13 +96,22 @@ namespace MazeSolver
             list[x2] = temp;
         }
 
+        /**
+         * O(log N)
+         * Ensures the array meets the heap criteria, 
+         * ensuring the tree created with the given 
+         * index at the root is a min heap. 
+         * 
+         **/
         private void minHeapify(int index)
         {
             int smallest = index;
+            //true if the given index has a left child, and that child is smaller than it
             if (size - 1 >= 2 * index && list[2 * index].CompareTo(list[smallest]) < 0)
             {
                 smallest = 2 * index;
             }
+            //true if the given index has a right child, and that child is smaller than it and the left child
             if (size - 1 >= (2 * index) + 1 && list[(2 * index) + 1].CompareTo(list[smallest]) < 0)
             {
                 smallest = 2 * index + 1;
--- MazeSolver/Node.cs
+++ MazeSolver/Node.cs
@@ -7,13 +7,31 @@ namespace MazeSolver
 {
     class Node : IComparable
     {
+        //x coordinate in the maze
         public Int32 x { get; set; }
+
+        //y coordinate in the maze
         public Int32 y { get; set; }
+
+        //x coordinate of the node reached immediately before this node 
         public Int32 parentX { get; set; }
+
+        //y coordinate of the node reached immediately before this node 
         public Int32 parentY { get; set; }
+
+        //blue = 3
+        //red = 2
+        //black = 1
+        //white = 0
         public byte cellType { get; }
+
+        //heuristic score of the node, used during the A* search
         public Int32 score { get; set; }
+
+        //number of nodes away from the starting node
         public Int32 steps { get; set; }
+
+        //whether or not this node has already been considered during the A* search
         public bool visited { get; set; }
 
         public Node(byte cellType, Int32 this_x, Int32 this_y, Int32 goal_x, Int32 goal_y)
@@ -21,16 +39,20 @@ namespace MazeSolver
             this.cellType = cellType;
             this.x = this_x;
             this.y = this_y;
+            //black nodes
             if (cellType == 1)
             {
                 this.score = Int32.MaxValue;
             }
+            //blue nodes
             else if (cellType == 3)
             {
                 this.score = 0;
             }
+            //White and red nodes
             else
             {
+                //heuristic score is the shortest path, regardless of walls, to the finishing node
                 this.score = Math.Abs(this_x - goal_x) + Math.Abs(this_y - goal_y);
             }
             visited = false;
